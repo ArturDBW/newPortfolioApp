5216e711b1015694297789aaa61edcb49469640b
Artur Wiśniewski <artixbtw@gmail.com>
2026-02-03 19:52:57 +0100

    create variable liStyles in Header.tsx

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -13,44 +13,24 @@ export default function Header() {
     return pathname.startsWith(href);
   };
 
+  const liStyles = `${isActive("/") ? "text-[#222222]" : "text-[#aaaaaa]"} transition-all hover:text-[#222222]`;
+
   return (
     <header className="sticky top-0 left-0 flex h-screen w-25 flex-col items-center justify-between border-r border-[#e6e6e6] bg-white py-12 text-[#011933]">
       <div className="text-3xl font-bold">dBw</div>
       <nav className="rotate-270 text-sm font-bold tracking-widest uppercase">
         <ul className="flex space-x-10">
           <Link href="/contact">
-            <li
-              className={`${
-                isActive("/contact") ? "text-[#222222]" : "text-[#aaaaaa]"
-              } transition-all hover:text-[#222222]`}
-            >
-              Contact
-            </li>
+            <li className={liStyles}>Contact</li>
           </Link>
           <Link href="/technologies">
-            <li
-              className={`${
-                isActive("/technologies") ? "text-[#222222]" : "text-[#aaaaaa]"
-              } transition-all hover:text-[#222222]`}
-            >
-              Technologies
-            </li>
+            <li className={liStyles}>Technologies</li>
           </Link>
           <Link href="/portfolio">
-            <li
-              className={`${
-                isActive("/portfolio") ? "text-[#222222]" : "text-[#aaaaaa]"
-              } transition-all hover:text-[#222222]`}
-            >
-              Portfolio
-            </li>
+            <li className={liStyles}>Portfolio</li>
           </Link>
           <Link href="/">
-            <li
-              className={`${isActive("/") ? "text-[#222222]" : "text-[#aaaaaa]"} transition-all hover:text-[#222222]`}
-            >
-              About
-            </li>
+            <li className={liStyles}>About</li>
           </Link>
         </ul>
       </nav>
