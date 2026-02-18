c3c1af8d8bc7738406ec832b7394e19e7f0190d0
Artur Wiśniewski <artixbtw@gmail.com>
2026-02-18 17:58:42 +0100

    fix wite up animation

--- frontend/app/technologies/page.tsx
+++ frontend/app/technologies/page.tsx
@@ -13,55 +13,55 @@ import IconTypeScript from "../icons/IconTypeScript";
 import IconVite from "../icons/IconVite";
 
 export default function Page() {
-  // const isActive = true;
+  const isActive = true;
+
   return (
-    // <>
-    //   {isActive && <div className="white-wipe" />}
-    //   <div className="bg-green-200">Technologies</div>
-    // </>
-    <div className="flex w-full">
-      <div className="flex-3 bg-white">
-        <div className="border-b border-[#e6e6e6] px-25 py-30">
-          <h2 className="text-5xl font-bold tracking-wider text-[#011933]">
-            Technologies
-          </h2>
-          <h5 className="mt-5 flex items-center text-sm tracking-widest text-[#8A6B0C] uppercase before:mr-3 before:h-px before:w-6 before:bg-[#8A6B0C] before:content-['']">
-            Updated: 10 February 2026
-          </h5>
+    <>
+      {isActive && <div className="white-wipe-out" />}
+      <div className="flex w-full">
+        <div className="flex-3 bg-white">
+          <div className="border-b border-[#e6e6e6] px-25 py-30">
+            <h2 className="text-5xl font-bold tracking-wider text-[#011933]">
+              Technologies
+            </h2>
+            <h5 className="mt-5 flex items-center text-sm tracking-widest text-[#8A6B0C] uppercase before:mr-3 before:h-px before:w-6 before:bg-[#8A6B0C] before:content-['']">
+              Updated: 10 February 2026
+            </h5>
+          </div>
+          <div className="border-b border-[#e6e6e6] px-25 py-20">
+            <p className="text-[#011933]">
+              Below are the technologies I`ve used in completed projects and am
+              ready to apply in a Junior Developer <br /> role. I`m always eager
+              to learn new tools and frameworks, so this list is constantly
+              evolving as I expand my skill set.
+            </p>
+          </div>
+          <div className="flex flex-wrap space-y-12 space-x-12 px-25 py-20">
+            <IconHTML />
+            <IconCSS />
+            <IconSass />
+            <IconTailwind />
+            <IconJavaScript />
+            <IconTypeScript />
+            <IconReact />
+            <IconNodeJS />
+            <IconExpress />
+            <IconMongoDB />
+            <IconVite />
+            <IconNextJS />
+            <IconFigma />
+          </div>
         </div>
-        <div className="border-b border-[#e6e6e6] px-25 py-20">
-          <p className="text-[#011933]">
-            Below are the technologies I`ve used in completed projects and am
-            ready to apply in a Junior Developer <br /> role. I`m always eager
-            to learn new tools and frameworks, so this list is constantly
-            evolving as I expand my skill set.
-          </p>
-        </div>
-        <div className="flex flex-wrap space-y-12 space-x-12 px-25 py-20">
-          <IconHTML />
-          <IconCSS />
-          <IconSass />
-          <IconTailwind />
-          <IconJavaScript />
-          <IconTypeScript />
-          <IconReact />
-          <IconNodeJS />
-          <IconExpress />
-          <IconMongoDB />
-          <IconVite />
-          <IconNextJS />
-          <IconFigma />
-        </div>
+        <div
+          className="flex-2 bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage: "url('/img/technologiesbgimage.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "25% center",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
       </div>
-      <div
-        className="flex-2 bg-cover bg-center bg-no-repeat"
-        style={{
-          backgroundImage: "url('/img/technologiesbgimage.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "25% center",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
-    </div>
+    </>
   );
 }
