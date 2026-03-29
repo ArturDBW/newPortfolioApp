3c3698e8f4a80b2d008e44d1bd5fd220d89c4151
Artur Wiśniewski <artixbtw@gmail.com>
2026-03-29 18:57:55 +0200

    reinstall node modules, delete package-lock.json from directory and fix bug with read file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,12 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./app/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+};
 
 export default withNextIntl(nextConfig);
